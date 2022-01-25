261f4775e63577428727c52b8b295ff7a9b2050d
Arthur <arthur_julio@gap.com>
2022-01-24 21:39:04 -0300

    A tokenizer implementation

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,10 @@
 from time import process_time
 from functools import wraps
 
+import pprint
+
+from tokenizer import tokenize
+
 def timer(msg="Exec time: {0:.7f}s"):
     def decorador(method):
         @wraps(method)
@@ -15,7 +19,7 @@ def timer(msg="Exec time: {0:.7f}s"):
     return decorador
 
 
-import pprint
+
 
 r_words = {
     'lambda':'LAMBDA', 'λ':'LAMBDA', 'let':'LET',
@@ -59,25 +63,28 @@ def prompt():
         run_line(line)
 
 
-def literal_val(val):
-    try:
-        return int(val)
-    except ValueError:
-        return float(val)
-    
+def literal_val(token):
+    if token[0]=='STR': return token[1]
+    if token[0]=='INT': return int(token[1])
+    if token[0]=='FLOAT': return float(token[1])
 
 def interpret(token_list,stop=False, stack=None, fun_map=std_lib_funs):
+    #breakpoint()
     if not stack: stack=[]
     if not token_list: return stack
     
     token = token_list.pop(0)
-    if token in fun_map:  # if function
-        stack.append(fun_map[token][1])
-        for _ in range(fun_map[token][0]):
-            if token_list[0] in fun_map:
-                stack.append(interpret(token_list, True))
-            else: 
-                stack.append(literal_val(token_list.pop(0)))
+    
+    if token[0]=='SYMB':
+        if token[1] in fun_map:
+            stack.append(fun_map[token[1]][1])
+            for _ in range(fun_map[token[1]][0]):
+                if token_list[0][1] in fun_map:
+                    stack.append(interpret(token_list, True))
+                else: 
+                    stack.append(literal_val(token_list.pop(0)))
+        else:
+            print('Function not implemented')
     else: # if literal
         stack.append(literal_val(token))
     if stop: 
@@ -87,6 +94,7 @@ def interpret(token_list,stop=False, stack=None, fun_map=std_lib_funs):
     return stack
 
 def apply(stack, scope=scope):
+    print(stack)
     for i, item in enumerate(stack):
         if isinstance(item, list):
             stack[i] = apply(stack[i])
@@ -98,14 +106,9 @@ def apply(stack, scope=scope):
 
 @timer()
 def run_line(line):
-    # TODO
-    # identify strings pre-split
-    # identify indentation pre-split
-    # identify parenthesis and commas pre-split
     
-    token_list = [token for token in line.split()]
-    if token_list[0] == '#': return
-    if token_list[0] == 'dir':
+    indent, token_list = tokenize(line)
+    if token_list[0][1] == 'dir':
         pprint.pprint(scope['cache'])
         return
         
---
+++ tokenizer.py
@@ -0,0 +1,43 @@
+import re
+
+def commented(line):
+    return line.split('#', 1)[0]
+
+def indented(line, r=re.compile('^\s+')):
+    length = r.match(line).span()[1] if r.match(line) else 0
+    return (length, line[length:])
+
+lit_str=re.compile('[rf]?(\".*?\"|\'.*?\')')
+
+lit_float=re.compile('[+-]?([0-9]*\.[0-9]+([eE][+-]?[0-9]*)?)')
+
+lit_int=re.compile('[+-]?'
+                       '(((0b)[0-1]+)'
+                       '|((0o)[0-7]+)'
+                       '|((0x)[0-9a-fA-F]+)'
+                       '|((0d)?[0-9]+))')
+
+
+def tokenize(line):
+    line = commented(line)
+    indent_qtd, line = indented(line)
+    
+    line = lit_str.split(line)
+    
+    sub_split = []
+    for item in line:
+        if lit_str.match(item):
+            sub_split.append(('STR',item))
+            continue
+        for i in item.split():
+            # this strategy is too agressive
+            i=i.replace('(','').replace(')','').replace(',','')
+            
+            label = 'SYMB'
+            if lit_float.match(i): label='FLOAT'
+            elif lit_int.match(i): label='INT'
+            
+            if i:
+                sub_split.append((label, i))
+    
+    return indent_qtd, sub_split
