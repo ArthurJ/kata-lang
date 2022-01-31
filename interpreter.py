801e3652fcc09893078d058684c2e8dcdafb7229
Arthur <arthur_julio@gap.com>
2022-01-31 13:14:14 -0300

    Testing pre-commit hooks

--- interpreter.py
+++ interpreter.py
@@ -4,6 +4,7 @@ from functools import wraps
 import pprint
 from rich.console import Console
 from rich.traceback import install
+
 install()
 
 from prompt_toolkit import PromptSession
@@ -20,37 +21,42 @@ def timer(msg="Exec time: {0:.7f}s"):
             ts = process_time()
             result = method(*args, **kw)
             te = process_time()
-            delta = te-ts
+            delta = te - ts
             print(msg.format(delta))
             return result
+
         return wrapper
+
     return decorador
+
 
 def tupleit(l):
     return tuple(map(tupleit, l)) if isinstance(l, (list, tuple)) else l
 
-#future -> (qtd_arg, {(type list):implementation})
+
+# future -> (qtd_arg, {(type list):implementation})
 std_lib_funs = {
-    '+': (2, lambda x,y: x+y),
-    '-': (2, lambda x,y: x-y),
-    '*': (2, lambda x,y: x*y),
-    '/': (2, lambda x,y: x/y),
-    '//':(2, lambda x,y:x//y),
-    '**':(2, lambda x,y:x**y),
-    '=': (2, lambda x,y:x==y),
-    '<=':(2, lambda x,y:x<=y),
-    '>=':(2, lambda x,y:x>=y),
-    '<': (2, lambda x,y: x<y),
-    '>': (2, lambda x,y: x>y),
-    'and': (2, lambda x,y: x and y),
-    'or': (2, lambda x,y: x or y),
-    'not': (1, lambda x: not x),
+    "+": (2, lambda x, y: x + y),
+    "-": (2, lambda x, y: x - y),
+    "*": (2, lambda x, y: x * y),
+    "/": (2, lambda x, y: x / y),
+    "//": (2, lambda x, y: x // y),
+    "**": (2, lambda x, y: x**y),
+    "=": (2, lambda x, y: x == y),
+    "<=": (2, lambda x, y: x <= y),
+    ">=": (2, lambda x, y: x >= y),
+    "<": (2, lambda x, y: x < y),
+    ">": (2, lambda x, y: x > y),
+    "and": (2, lambda x, y: x and y),
+    "or": (2, lambda x, y: x or y),
+    "not": (1, lambda x: not x),
 }
 
+
 class Scope:
-    def __init__(self, values=None,
-                 functions=std_lib_funs, cache=None,
-                 outer_scope=None) -> None:
+    def __init__(
+        self, values=None, functions=std_lib_funs, cache=None, outer_scope=None
+    ) -> None:
         self.__cache__ = cache or {}
         self.__values__ = values or {}
         self.__functions__ = functions or {}
@@ -61,7 +67,7 @@ class Scope:
 
     @property
     def functions(self):
-        # As funções podem mudar, 
+        # As funções podem mudar,
         # mas os resultados do lambda no cache se mantém pois o lambda ao qual se referem é a chave
         # assim não é necessário atualizar o cache
         return self.__functions__
@@ -75,49 +81,62 @@ class Scope:
         return self.__cache__
 
     def __contains__(self, value):
-        return value in self.cache \
-                or value in self.__functions__ \
-                or value in self.__values__
+        return (
+            value in self.cache
+            or value in self.__functions__
+            or value in self.__values__
+        )
 
     def __str__(self):
-        return 'Functions:\n '\
-                + pprint.pformat(self.functions).replace('\n','\n ')\
-                + '\n\nCache:\n '\
-                + pprint.pformat(self.__cache__).replace('\n','\n ')\
-                + '\n\nValues:\n '\
-                + pprint.pformat(self.__values__).replace('\n','\n ')\
-                + '\n'
+        return (
+            "Functions:\n "
+            + pprint.pformat(self.functions).replace("\n", "\n ")
+            + "\n\nCache:\n "
+            + pprint.pformat(self.__cache__).replace("\n", "\n ")
+            + "\n\nValues:\n "
+            + pprint.pformat(self.__values__).replace("\n", "\n ")
+            + "\n"
+        )
 
 
 scope = Scope()
 
+
 def prompt():
-    session =  PromptSession()
+    session = PromptSession()
     print_fun = Console().print
-    breakpoint()
+    # breakpoint()
 
     while True:
         try:
-            line = session.prompt('-> ', lexer=PygmentsLexer(LispLexer))
+            line = session.prompt("-> ", lexer=PygmentsLexer(LispLexer))
         except EOFError:
             break
-        if line: run_line(line, print_fun)
+        if line:
+            run_line(line, print_fun)
 
 
 def literal_val(token):
-    if token[0]=='STR': return token[1]
-    if token[0]=='INT': return int(token[1])
-    if token[0]=='FLOAT': return float(token[1])
-    if token[0]=='BOOL': return token[1]=='True'
-    if token[1] in scope: return scope[token[1]]
-    raise ValueError(f'{token[1]} not know')
+    if token[0] == "STR":
+        return token[1]
+    if token[0] == "INT":
+        return int(token[1])
+    if token[0] == "FLOAT":
+        return float(token[1])
+    if token[0] == "BOOL":
+        return token[1] == "True"
+    if token[1] in scope:
+        return scope[token[1]]
+    raise ValueError(f"{token[1]} not know")
+
 
 def is_literal(val):
-    if isinstance(val, tuple): return val[0] in ['STR', 'INT', 'FLOAT', 'BOOL']
+    if isinstance(val, tuple):
+        return val[0] in ["STR", "INT", "FLOAT", "BOOL"]
     return False
 
 
-'''
+"""
 0
 + 1 1
 + 1 - 5 4
@@ -129,61 +148,71 @@ let k False
 k
 or k True
 and True not k
-'''
+"""
+
+
 def interpret(token_list, scope=scope):
-    if not token_list: return []
-    #breakpoint()
+    if not token_list:
+        return []
+    # breakpoint()
     root = token_list.pop(0)
-    if root[0]=='LET':
-        return ['LET', interpret(token_list, scope)]
-    elif is_literal(root): return root
+    if root[0] == "LET":
+        return ["LET", interpret(token_list, scope)]
+    elif is_literal(root):
+        return root
     elif root[1] in scope.functions:
-        arg_qtd, _lambda= scope.functions[root[1]]
+        arg_qtd, _lambda = scope.functions[root[1]]
         args = []
         for _ in range(arg_qtd):
             args.append(interpret(token_list, scope))
         return [_lambda, args]  # [root, args]
     elif root[1] in scope.values:
         return scope.values[root[1]]
-    elif root[0]=='SYMB': return [root, interpret(token_list, scope)]
+    elif root[0] == "SYMB":
+        return [root, interpret(token_list, scope)]
 
-    raise ValueError('Something went wrong with your expression')
+    raise ValueError("Something went wrong with your expression")
 
 
-def bind(symb, val, scope=scope): # sempre guarda expressões
-    if not symb[0]=='SYMB': raise ValueError('To bind a symbol is needed')
-    scope.values[symb[1]]=val
+def bind(symb, val, scope=scope):  # sempre guarda expressões
+    if not symb[0] == "SYMB":
+        raise ValueError("To bind a symbol is needed")
+    scope.values[symb[1]] = val
 
 
 def eval(stack, scope=scope):
-    #print(stack)
-    if not stack: return ''
-    if is_literal(stack): return literal_val(stack)
-    if stack[0]=='LET':
+    # print(stack)
+    if not stack:
+        return ""
+    if is_literal(stack):
+        return literal_val(stack)
+    if stack[0] == "LET":
         bind(*stack[1], scope)
         return stack
     if callable(stack[0]):
-        scope.cache[stack] = \
-            scope.cache.get(stack,
-                            stack[0](*[eval(x, scope) for x in stack[1]]))
+        scope.cache[stack] = scope.cache.get(
+            stack, stack[0](*[eval(x, scope) for x in stack[1]])
+        )
         return scope.cache[stack]
-    if stack[0]=='SYMB' and stack[1] not in scope.values:
-        raise ValueError('Unknown symbol')
-    raise Exception('Something wrong is not right')
+    if stack[0] == "SYMB" and stack[1] not in scope.values:
+        raise ValueError("Unknown symbol")
+    raise Exception("Something wrong is not right")
+
 
 @timer()
 def run_line(line, print_fun=None):
-    if not print_fun: print_fun=print
+    if not print_fun:
+        print_fun = print
     indent, token_list = tokenize(line)
-    if token_list[0][1] == 'dir':
+    if token_list[0][1] == "dir":
         print_fun(scope)
         return
     try:
         stack = tupleit(interpret(token_list))
         print_fun(eval(stack))
     except Exception as e:
-        print_fun(f'Error: {e}')
+        print_fun(f"Error: {e}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     prompt()
--- tokenizer.py
+++ tokenizer.py
@@ -1,21 +1,23 @@
 import re
 
-def commented(line):
-    return line.split('#', 1)[0]
 
-def indented(line, r=re.compile('^\s+')):
+def commented(line):
+    return line.split("#", 1)[0]
+
+
+def indented(line, r=re.compile("^\s+")):
     length = r.match(line).span()[1] if r.match(line) else 0
     return (length, line[length:])
 
-lit_str=re.compile('[rf]?(\".*?\"|\'.*?\')')
 
-lit_float=re.compile('[+-]?([0-9]*\.[0-9]+([eE][+-]?[0-9]*)?)')
+lit_str = re.compile("[rf]?(\".*?\"|'.*?')")
 
-lit_int=re.compile('[+-]?'
-                       '(((0b)[0-1]+)'
-                       '|((0o)[0-7]+)'
-                       '|((0x)[0-9a-fA-F]+)'
-                       '|((0d)?[0-9]+))')
+lit_float = re.compile("[+-]?([0-9]*\.[0-9]+([eE][+-]?[0-9]*)?)")
+
+lit_int = re.compile(
+    "[+-]?"
+    "(((0b)[0-1]+)" + "|((0o)[0-7]+)" + "|((0x)[0-9a-fA-F]+)" + "|((0d)?[0-9]+))"
+)
 
 
 def tokenize(line):
@@ -27,17 +29,21 @@ def tokenize(line):
     sub_split = []
     for item in line:
         if lit_str.match(item):
-            sub_split.append(('STR',item))
+            sub_split.append(("STR", item))
             continue
         for i in item.split():
             # this strategy is too agressive, TODO change it
-            i=i.replace('(','').replace(')','').replace(',','')
+            i = i.replace("(", "").replace(")", "").replace(",", "")
 
-            label = 'SYMB'
-            if i=='let': label='LET'
-            elif i in ['True','False']: label='BOOL'
-            elif lit_float.match(i): label='FLOAT'
-            elif lit_int.match(i): label='INT'
+            label = "SYMB"
+            if i == "let":
+                label = "LET"
+            elif i in ["True", "False"]:
+                label = "BOOL"
+            elif lit_float.match(i):
+                label = "FLOAT"
+            elif lit_int.match(i):
+                label = "INT"
 
             if i:
                 sub_split.append((label, i))
@@ -45,28 +51,35 @@ def tokenize(line):
     return indent_qtd, sub_split
 
 
-
 # para o futuro
 r_words = {
-    'let':'LET',
-    'lambda':'LAMBDA', 
-    'λ':'LAMBDA', 
-    '(':'LEFT_PAREN', 
-    ')':'RIGHT_PAREN',
-    ',':'COMMA', 
-    '.':'DOT', 
-    ':':'COLON',
-    'as':'AS',
-    'with':'WITH', 
-    '?':'QUESTION_MARK',
-    'otherwise':'OTHERWISE' 
+    "let": "LET",
+    "lambda": "LAMBDA",
+    "λ": "LAMBDA",
+    "(": "LEFT_PAREN",
+    ")": "RIGHT_PAREN",
+    ",": "COMMA",
+    ".": "DOT",
+    ":": "COLON",
+    "as": "AS",
+    "with": "WITH",
+    "?": "QUESTION_MARK",
+    "otherwise": "OTHERWISE",
 }
 
 token_type = [
-    'LAMBDA', 'LET',
-    'IDENTIFIER', 'STRING', 'NUMBER', 
-    'LEFT_PAREN', 'RIGHT_PAREN',
-    'COMMA', 'DOT', 'COLON',
-    'OTHERWISE', 'WITH', 'AS',
-    'QUESTION_MARK'
+    "LAMBDA",
+    "LET",
+    "IDENTIFIER",
+    "STRING",
+    "NUMBER",
+    "LEFT_PAREN",
+    "RIGHT_PAREN",
+    "COMMA",
+    "DOT",
+    "COLON",
+    "OTHERWISE",
+    "WITH",
+    "AS",
+    "QUESTION_MARK",
 ]
