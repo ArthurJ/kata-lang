a222707281bc02c4819e38e71226894d0e33caf6
Arthur <arthur_julio@gap.com>
2022-04-23 09:24:38 -0300

    Unificando TYPED_VAL ao TYPED_SYMBOL, não faz sentido mante-los separados por enquanto

--- tokenizer.py
+++ tokenizer.py
@@ -5,11 +5,16 @@ import re
 token_patterns = {
     "SHEBANG": re.compile(r"#(!.*$)"),
     "COMMENT": re.compile(r"#.*$"),
+    #
     "INDENT": re.compile(r"(^[\t ]+)"),
+    #
     "STR": re.compile(r"(?:(?<=^)|(?<=\s))'(.*?)'(?:(?=\s)|(?=$))"),
-    # 'R_STR':re.compile(r"r'(.*?)'"), #TODO ?
     "F_STR": re.compile(r"(?:(?<=^)|(?<=\s))f'(.*?)'(?:(?=\s)|(?=$))"),
+    #
     "LET": re.compile(r"(?:(?<=^)|(?<=\s))(let)(?=\s)"),
+    # "LAMBDA": re.compile(r"(?:(?<=^)|(?<=\s))(lambda)(?=\s)"),
+    # "ACTION": re.compile(r"(?:(?<=^)|(?<=\s))(action)(?=\s)"),
+    #
     "INT": re.compile(
         r"(?:(?<=^)|(?<=\s))([+-]?(?:\d_?)+(?:[eE][+-]?(?:\d_?)+)?)(?:(?=\s)|(?=$))"  # noqa: E501
     ),  # INT base 10, INT eng
@@ -25,11 +30,18 @@ token_patterns = {
     "HEX": re.compile(
         r"(?:(?<=^)|(?<=\s))([+-]?0x(?:[\da-fA-F]_?)+)(?:(?=\s)|(?=$))"
     ),  # noqa: E501
-    "SYMBOL": re.compile(r"(?:(?<=^)|(?<=\s))([a-z]\w*)(?:(?=\s)|(?=$))"),
-    "TYPED_SYMBOL": re.compile(
-        r"(?:(?<=^)|(?<=\s))([a-z]\w*::\w+)(?:(?=\s)|(?=$))"
+    #
+    # NÃO PONHA O CARRO NA FRENTE DOS BOIS
+    # "QUESTION_MARK": re.compile(
+    #    r"(?:(?<=^)|(?<=\s))(\?)(?:(?=\s)|(?=$))"),
+    # "UNDERSCORE": re.compile(
+    #    r"(?:(?<=^)|(?<=\s))(_)(?:(?=\s)|(?=$))"),
+    #
+    "TYPED_SYMB": re.compile(r"(?:(?<=^)|(?<=\s))(\S+::\S+)(?:(?=\s)|(?=$))"),
+    "SYMBOL": re.compile(
+        r"(?:(?<=^)|(?<=\s))([^\sA-Z0-9\+:\.\-]+)(?:(?=\s)|(?=$))"
     ),  # noqa: E501
-    "TYPED_VAL": re.compile(r"(?:(?<=^)|(?<=\s))(.+::\w+)(?:(?=\s)|(?=$))"),
+    #
     "TYPE": re.compile(r"(?:(?<=^)|(?<=\s))([A-Z][a-z]+)(?:(?=\s)|(?=$))"),
     "INTERFACE": re.compile(
         r"(?:(?<=^)|(?<=\s))([A-Z][A-Z0-9]*)(?:(?=\s)|(?=$))"
@@ -46,8 +58,6 @@ class Token:
 def replacer(str_tok_list, pattern, tok_type):
     for idx, item in enumerate(str_tok_list):
         if not isinstance(item, Token):
-            # print('replacer input->', str_tok_list)
-            # print('replacer item->', item)
             if found := pattern.findall(item):
                 str_tok_list[idx] = [
                     Token(tok_type, i) if i in found else i
@@ -69,38 +79,35 @@ def flatten(itr):
 
 
 def typed_parser(thing):
-    if isinstance(thing, str):
+    if isinstance(thing, str) or thing.token_type != "TYPED_SYMB":
         return thing
-    if thing.token_type not in ("TYPED_SYMBOL", "TYPED_VAL"):
-        return thing
-
-    val_and_type = thing.value.split("::")
+    typed_value = thing.value.split("::")
+    # typed_value.reverse()
     for tok_type in token_patterns.keys():
-        val_and_type = list(
-            replacer(val_and_type, token_patterns[tok_type], tok_type)
+        typed_value = list(
+            replacer(typed_value, token_patterns[tok_type], tok_type)
         )  # noqa: E501
-    return val_and_type
+    return typed_value
 
 
 def tokenize_code(code):
     lines = [[line] for line in code]
     for tok_type in token_patterns.keys():
         for idx, line in enumerate(lines):
-            # print('\ttokenize_code antes->', lines[idx])
             lines[idx] = list(
                 replacer(line, token_patterns[tok_type], tok_type)
             )  # noqa: E501
-            # print('\ttokenize_code depois->',lines[idx])
-            if tok_type in ("TYPED_SYMBOL", "TYPED_VAL"):
+            if tok_type == "TYPED_SYMB":
                 lines[idx] = list(
                     flatten([typed_parser(tok) for tok in lines[idx]])
                 )  # noqa: E501
-    # VER RESULTADOS
-    for line in lines:
-        print(line)
+    return lines
 
 
 # -----------------------------------------------------------------------------
 
-with open("tokenizers_test_code.txt", "r") as test_code_file:
-    tokenize_code(test_code_file.read().splitlines())
+if __name__ == "__main__":
+    from pprint import pprint
+
+    with open("tokenizers_test_code.txt", "r") as test_code_file:
+        pprint(tokenize_code(test_code_file.read().splitlines()))
